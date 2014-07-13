1de95a186f11fffead9dfacf6b3bdf68ed547261
Ian Juma <ian.juma@yahoo.com>
2014-07-14 01:17:55 +0300

    app

--- app/app.py
+++ app/app.py
@@ -318,7 +318,73 @@ def profile(username):
         return resp
 
     return render_template(
-        'profile.html', name=name, email=email, smscode=smscode,
+        'profile2.html', name=name, email=email, smscode=smscode,
+        state=state, username=username, mobileNo=mobileNo)
+
+
+
+@app.route('/payments/<username>/', methods=['POST', 'GET'])
+def payments(username):
+
+    if request.method == 'POST':
+
+        if not request.json:
+            abort(400)
+
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400)
+
+        password = request.json.get('password')
+        smsdata = request.json.get('smsdata')
+        email = request.json.get('email')
+        dob = request.json.get('dob')
+        username = request.json.get('username')
+        state = request.json.get('state')
+        mobileNo = request.json.get('mobileNo')
+
+        if mobileNo.startswith('0'):
+            mobileNo = mobileNo[1:]
+
+        if mobileNo.startswith('+254'):
+            mobileNo = mobileNo[4:]
+
+        try:
+            user = r.table(
+                'UsersInfo').get(str(username)).update({"email": email, "smscode": smsdata,
+                                                        "state": state, "dob": dob, "mobileNo": mobileNo}).run(g.rdb_conn)
+
+            resp = make_response(jsonify({"OK": "User Updated"}), 202)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        except RqlError:
+            logging.warning(
+                'DB code verify failed on /profile/api/' + username)
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+    try:
+        user = r.table('UsersInfo').get(str(username)).run(g.rdb_conn)
+
+        name = str(user['username'])
+        state = str(user['state'])
+        smscode = str(user['smscode'])
+        password = str(user['password'])
+        email = str(user['email'])
+        mobileNo = str(user['mobileNo'])
+
+    except RqlError:
+        logging.warning('DB code verify failed on /profile/' + mobileNo)
+        resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+        resp.headers['Content-Type'] = "application/json"
+        resp.cache_control.no_cache = True
+        return resp
+
+    return render_template(
+        'payments.html', name=name, email=email, smscode=smscode,
         state=state, username=username, mobileNo=mobileNo)
 
 
@@ -327,7 +393,7 @@ def tasks(username):
     # task = RegistrationForm(request.form)
     # get mobileNo
     # check if no exists
-    return render_template('task.html', username=username)
+    return render_template('createTask.html', username=username)
 
 
 @app.route('/adminTasks/', methods=['POST', 'GET'])
@@ -380,7 +446,7 @@ def getAdminTasks():
 @app.route('/allTasks/<username>/', methods=['POST', 'GET'])
 def getAllTasks(username):
     username = username
-    return render_template('allTasks.html', username=username)
+    return render_template('allTasks2.html', username=username)
 
 
 @app.route('/api/getTasks/', methods=['POST', 'GET'])
@@ -432,7 +498,7 @@ def taskInfo(username, task_id):
         resp.cache_control.no_cache = True
         return resp
 
-    return render_template('editTask.html', task_category=task_category, task_urgency=task_urgency,
+    return render_template('editTask2.html', task_category=task_category, task_urgency=task_urgency,
         task_desc=task_desc, task_title=task_title, due_date=due_date, username=username, task_id=task_id)
 
 
@@ -638,7 +704,7 @@ def addNewsLetter():
     return resp
 
 
-@app.route('/api/removeUser/', methods=['DELETE'])
+@app.route('/api/removeUser/', methods=['POST'])
 def removeUser():
     if not request.json:
         abort(400)
