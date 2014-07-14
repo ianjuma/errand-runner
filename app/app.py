60634a896dc1c5ff8beec20d751adbcc71308932
Ian Juma <ian.juma@yahoo.com>
2014-07-14 23:13:29 +0300

    app update

--- app/app.py
+++ app/app.py
@@ -870,8 +870,8 @@ def gone(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    import newrelic.agent
-    newrelic.agent.initialize('conf/newrelic.ini')
+    # import newrelic.agent
+    # newrelic.agent.initialize('conf/newrelic.ini')
     app.run('0.0.0.0', port=port, debug=False, threaded=False)
     # app.run(port=8000, debug=True, host='0.0.0.0')
     # this can be omitted if using gevent wrapped around gunicorn
