fdc28587685963202ad9d4f2de8fb2bbe227d90f
Ian Juma <ian.juma@yahoo.com>
2014-07-17 00:09:48 +0300

    userops

---
+++ app/userOps/profile_admin.py
@@ -0,0 +1,290 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# res/ rep cycle
+
+from app import app
+from app import r
+from app import g
+from app import logging
+from app import salt
+
+
+from flask import (render_template, url_for)
+from flask import redirect, make_response, Flask
+from flask import Response, jsonify
+from flask import abort, request
+from flask import session, g
+import pickle
+from datetime import timedelta
+from uuid import uuid4
+from redis import Redis
+from redis import StrictRedis
+from werkzeug.datastructures import CallbackDict
+from flask.sessions import SessionInterface, SessionMixin
+from json import dumps
+
+import os
+import logging
+import hashlib
+import uuid
+from random import randint
+
+import time
+from datetime import datetime
+
+
+@app.route('/profile/<username>/', methods=['POST', 'GET'])
+def profile(username):
+
+    if request.method == 'POST':
+
+        if not request.json:
+            abort(400)
+
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400)
+
+        password = request.json.get('password')
+        smsdata = request.json.get('smsdata')
+        email = request.json.get('email')
+        dob = request.json.get('dob')
+        username = request.json.get('username')
+        state = request.json.get('state')
+        mobileNo = request.json.get('mobileNo')
+
+        if mobileNo.startswith('0'):
+            mobileNo = mobileNo[1:]
+
+        if mobileNo.startswith('+254'):
+            mobileNo = mobileNo[4:]
+
+        try:
+            user = r.table(
+                'UsersInfo').get(str(username)).update({"email": email, "smscode": smsdata,
+                                                        "state": state, "dob": dob, "mobileNo": mobileNo}).run(g.rdb_conn)
+
+            resp = make_response(jsonify({"OK": "User Updated"}), 202)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        except RqlError:
+            logging.warning(
+                'DB code verify failed on /profile/api/' + username)
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+    try:
+        user = r.table('UsersInfo').get(str(username)).run(g.rdb_conn)
+
+        name = str(user['username'])
+        state = str(user['state'])
+        smscode = str(user['smscode'])
+        password = str(user['password'])
+        email = str(user['email'])
+        mobileNo = str(user['mobileNo'])
+
+    except RqlError:
+        logging.warning('DB code verify failed on /profile/' + mobileNo)
+        resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+        resp.headers['Content-Type'] = "application/json"
+        resp.cache_control.no_cache = True
+        return resp
+
+    return render_template(
+        'profile2.html', name=name, email=email, smscode=smscode,
+        state=state, username=username, mobileNo=mobileNo)
+
+
+
+@app.route('/payments/<username>/', methods=['POST', 'GET'])
+def payments(username):
+
+    if request.method == 'POST':
+
+        if not request.json:
+            abort(400)
+
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400)
+
+        password = request.json.get('password')
+        smsdata = request.json.get('smsdata')
+        email = request.json.get('email')
+        dob = request.json.get('dob')
+        username = request.json.get('username')
+        state = request.json.get('state')
+        mobileNo = request.json.get('mobileNo')
+
+        if mobileNo.startswith('0'):
+            mobileNo = mobileNo[1:]
+
+        if mobileNo.startswith('+254'):
+            mobileNo = mobileNo[4:]
+
+        try:
+            user = r.table(
+                'UsersInfo').get(str(username)).update({"email": email, "smscode": smsdata,
+                                                        "state": state, "dob": dob, "mobileNo": mobileNo}).run(g.rdb_conn)
+
+            resp = make_response(jsonify({"OK": "User Updated"}), 202)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        except RqlError:
+            logging.warning(
+                'DB code verify failed on /profile/api/' + username)
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+    try:
+        user = r.table('Payments').get(str(username)).run(g.rdb_conn)
+        username = str(user['username'])
+        credit = str(user['credit_available'])
+
+    except RqlError:
+        logging.warning('DB code verify failed on /payments/' + mobileNo)
+        resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+        resp.headers['Content-Type'] = "application/json"
+        resp.cache_control.no_cache = True
+        return resp
+
+    return render_template(
+        'payments.html', username=username, credit=credit)
+
+
+@app.route('/api/removeUser/', methods=['POST'])
+def removeUser():
+    if not request.json:
+        abort(400)
+
+    if request.headers['Content-Type'] != 'application/json':
+        abort(400)
+
+    password = request.json.get('password')
+    username = request.json.get('username')
+
+    # mobile no is the id - primary key
+    try:
+        r.table('UsersInfo').get(username).delete().run(g.rdb_conn)
+    except RqlError:
+        logging.warning('DB remove user failed on /api/removeUser')
+
+    resp = make_response(jsonify({'OK': 'Content Removed'}), 202)
+    resp.headers['Content-Type'] = "application/json"
+    resp.cache_control.no_cache = True
+    return resp
+
+
+@app.route('/api/UserInfo/', methods=['POST'])
+def addUser():
+    if not request.json:
+        abort(400)
+
+    if request.headers['Content-Type'] != 'application/json':
+        abort(400)
+
+    # get JSON engine params
+    fname = request.json.get('fname')
+    lname = request.json.get('lname')
+    username = request.json.get('username')
+    mobileNo = request.json.get('mobileNo')  # this <- id
+    state = request.json.get('state')
+    location = request.json.get('location')
+    email = request.json.get('email')
+
+    # no id just work with mobileNo - easier
+    # we'll send a text message enter code!
+
+    if mobileNo.startswith('0'):
+        mobileNo = mobileNo[1:]
+
+    if mobileNo.startswith('+254'):
+        mobileNo = mobileNo[4:]
+
+    try:
+        r.table('UsersInfo').insert({
+            'fname': fname,
+            'lname': lname,
+            'mobileNo': mobileNo,
+            'email': email,
+            'state': state,
+            'userVerified': 'False',
+            'location': location
+        }).run(g.rdb_conn)
+    except RqlError:
+        logging.warning('DB could not write on /api/adduser')
+
+    resp = make_response(jsonify({"OK": "Content Saved"}), 202)
+    resp.headers['Content-Type'] = "application/json"
+    resp.cache_control.no_cache = True
+    return resp
+
+
+# credit API
+@app.route('/api/credit/<username>/', methods=['POST', 'GET'])
+def credit(username):
+
+    if request.method == 'GET':
+
+        if not request.json:
+            abort(400)
+
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400)
+
+        password = request.json.get('password')
+        username = request.json.get('username')
+
+        try:
+            user = r.table(
+                'Payments').get(str(username)).pluck('credit_available').run(g.rdb_conn)
+
+            credit = json.dumps(user)
+            resp = make_response(jsonify(credit), 202)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        except RqlError:
+            logging.warning(
+                'DB code verify failed on /api/credit' + username)
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+    if request.method == 'POST':
+
+        if not request.json:
+            abort(400)
+
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400)
+
+        password = request.json.get('password')
+        username = request.json.get('username')
+
+        try:
+            user = r.table(
+                'Payments').get(str(username)).pluck('credit_available').run(g.rdb_conn)
+
+            resp = make_response(jsonify({"OK": "User Updated"}), 202)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        except RqlError:
+            logging.warning(
+                'DB code verify failed on /api/credit' + username)
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
---
+++ app/userOps/web_errors.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# res/ rep cycle
+from app import app
+from app import r
+from app import g
+from app import logging
+from app import salt
+
+
+from flask import (render_template, url_for)
+from flask import redirect, make_response, Flask
+from flask import Response, jsonify
+from flask import abort, request
+from flask import session, g
+import pickle
+from datetime import timedelta
+from uuid import uuid4
+from redis import Redis
+from redis import StrictRedis
+from werkzeug.datastructures import CallbackDict
+from flask.sessions import SessionInterface, SessionMixin
+from json import dumps
+
+import os
+import logging
+import hashlib
+import uuid
+from random import randint
+
+from datetime import datetime
+
+# Basic Error handlers
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify({"Error 404":
+                                  "Not Found"}), 404)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return make_response(jsonify({"Error 400":
+                                  "Bad request"}), 400)
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return make_response(jsonify({"Error 500":
+                                  "Internal Server Error"}), 500)
+
+
+@app.errorhandler(408)
+def timeout(e):
+    return make_response(jsonify({"Error 408":
+                                  "Request Timeout"}), 408)
+
+
+@app.errorhandler(405)
+def invalidMethod(e):
+    return make_response(jsonify({"Error 405":
+                                  "Invalid Request Method"}), 405)
+
+
+@app.errorhandler(410)
+def gone(e):
+    return make_response(jsonify({"Error 410":
+                                  "Resource is Gone"}), 410)
