e1bd2806a327099b5da87ead71717251c2a2eca0
Ian Juma <ian.juma@yahoo.com>
2014-07-19 00:18:17 +0300

    mail

--- app/mail/sendMail.py
+++ app/mail/sendMail.py
@@ -8,19 +8,20 @@ from sendgrid import SendGridError, SendGridClientError, SendGridServerError
 import logging
 logging.basicConfig(filename='SendMail.log', level=logging.DEBUG)
 
-sg = sendgrid.SendGridClient('IanJuma', 'obamanation2008', raise_errors=True)
+sg = sendgrid.SendGridClient('app27418636@heroku.com', 'w4do409h', raise_errors=True)
 
 
 def sendMail(to, mail, username):
     try:
         message = sendgrid.Mail()
-        message.add_to(username + "<" + mail + ">")
+        message.add_to(to)
         message.set_subject('LinkUs')
         message.set_html('Body')
         message.set_text(mail)
         message.set_from('LinkUs <linkus@gmail.com>')
 
-        status, msg = sg.send(message)
+        # status, msg = sg.send(message)
+        sg.send(message)
 
     except SendGridClientError as e:
         logging.warning('Mail failed Client Error %s' % str(e))
