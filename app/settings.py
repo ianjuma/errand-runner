e9c0281ba8bf9d1126fe22c9ac2767bc9eb755d8
Ian Juma <ian.juma@yahoo.com>
2014-07-28 00:08:12 +0300

    thread enable

--- app/settings.py
+++ app/settings.py
@@ -4,10 +4,10 @@ secret = os.urandom(24)
 
 
 DEBUG=True
-SECRET_KEY='secret'
+SECRET_KEY=secret
 CSRF_ENABLED=True
 CSRF_SESSION_LKEY='dev_key_h8asSNJ9s9=+'
-
+THREAD = True
 
 CLIENT_ID = '20567748533.apps.googleusercontent.com'
 CLIENT_SECRET = '82Y0LzHkrfXlep81sVLWIWh2'
