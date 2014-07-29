0b45548e1804420e9b7a51ae55c53472c84fd4f5
Ian Juma <ian.juma@yahoo.com>
2014-07-29 15:37:27 +0300

    fab update

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,26 @@
+from fabric.api import *
+
+env.user = 'root'
+env.hosts = ['188.226.195.158']
+
+
 def setup_server():
-    pass
+    run('mkdir /tmp/TaskWetu')
+    with cd('tmp/TaskWetu'):
+        run('pip install -r requirements.txt')
+        prepare_deploy()
+        deploy()
+        run('gunicorn -c config-gunicorn.py app:app')
+
 
 def prepare_deploy():
-	app_dir = '/root/TaskWetu/'
-    local("apt-get update && apt-get dist-upgrade -y")
-    local("")
+	app_dir = '/tmp/TaskWetu/'
+    local("apt-get update && apt-get -y dist-upgrade")
+
+
+def restartNginx():
+    sudo('service nginx restart')
+
+
+def deploy():
+    run('gunicorn -c config-gunicorn.py app:app')
