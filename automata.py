9e9c2bcc31659c7eca5deec164ef37bd1fe03990
Ian Juma <ian.juma@yahoo.com>
2014-08-11 22:16:04 +0300

    automata

--- automata.py
+++ automata.py
@@ -7,9 +7,9 @@ github_username = 'ianjuma'
 
 clean = pexpect.spawn('fab clean')
 clean.expect('Passphrase for private key:')
-clean.send(secret)
+clean.sendline(secret)
 
-deploy = pexpect.spawn('fab deploy:%s' %(version,))
+deploy = pexpect.spawn('fab deploy:%s' % (version,))
 deploy.expect('Passphrase for private key:')
 deploy.sendline(secret)
 deploy.expect("Username for 'https://github.com':")
