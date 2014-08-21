c4dfc31b7ab9f27c56eaf9d385ff54ddb0785e5e
Ian Juma <ian.juma@yahoo.com>
2014-08-21 13:12:28 +0300

    tasks fix email notification

--- app/taskOps/mail/sendMail.py
+++ app/taskOps/mail/sendMail.py
@@ -48,3 +48,26 @@ def passwordReset(to, newpassword):
         logging.warning('Mail failed Client Error %s' % str(e))
     except SendGridServerError as e:
         logging.warning('Mail failed Server Error %s' % str(e))
+
+
+def new_task_message(to, mail, username):
+    try:
+        to_send = "New Task has been created by user %s" %(username)
+        logo = '<img src="http://188.226.195.158/static/ico/taskwetu_logo.png"/>'
+
+        html = "<h3> %s </h3><br><p> %s </p>" %(logo, to_send)
+
+        message = sendgrid.Mail()
+        message.add_to(to)
+        message.set_subject('New Task Created')
+        message.set_html(html)
+        message.set_text( str(to_send) )
+        message.set_from('TaskWetu <taskkwetu@gmail.com>')
+
+        # status, msg = sg.send(message)
+        sg.send(message)
+
+    except SendGridClientError as e:
+        logging.warning('Mail failed Client Error %s' % str(e))
+    except SendGridServerError as e:
+        logging.warning('Mail failed Server Error %s' % str(e))
