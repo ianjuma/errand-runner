33ab8c23c2411aa45979a961d1f2b1bb51346be2
Ian Juma <ian.juma@yahoo.com>
2014-08-22 08:56:08 +0300

    payments

--- app/userOps/payments/process_payments.py
+++ app/userOps/payments/process_payments.py
@@ -8,26 +8,26 @@ pesapal.testing = False
 
 
 ### post a direct order
-
 post_params = {
-  'oauth_callback': 'http://188.226.195.158/billing/'
+    'oauth_callback': 'http://taskwetu.com/payments_pesapal'
 }
 request_data = {
-  'Amount': '100',
-  'Description': 'E-book purchase',
-  'Type': 'MERCHANT',
-  'Reference': '12erwe',
-  'PhoneNumber': '0701435178'
+    'Amount': '100',
+    'Description': 'E-book purchase', # task_cat
+    'Type': 'MERCHANT',
+    'Reference': '12erwe', # rand no - stored - task_id
+    'PhoneNumber': '0701435178'
 }
 # build url to redirect user to confirm payment
+# place url in view
 url = pesapal.postDirectOrder(post_params, request_data)
 
 
 ### get order status
-
+### get from url
 post_params = {
-  'pesapal_merchant_reference': '000',
-  'pesapal_transaction_tracking_id': '000'
+    'pesapal_merchant_reference': '000',
+    'pesapal_transaction_tracking_id': '000'
 }
 url = pesapal.queryPaymentStatus(post_params)
 response = urllib2.urlopen(url)
@@ -37,7 +37,7 @@ print response.read()
 ### get order status by ref
 
 post_params = {
-  'pesapal_merchant_reference': '000'
+    'pesapal_merchant_reference': '000'
 }
 url = pesapal.queryPaymentStatusByMerchantRef(post_params)
 response = urllib2.urlopen(url)
@@ -47,8 +47,8 @@ print response.read()
 ### get detailed order status
 
 post_params = {
-  'pesapal_merchant_reference': '000',
-  'pesapal_transaction_tracking_id': '000'
+    'pesapal_merchant_reference': '000',
+    'pesapal_transaction_tracking_id': '000'
 }
 url = pesapal.queryPaymentDetails(post_params)
 response = urllib2.urlopen(url)
