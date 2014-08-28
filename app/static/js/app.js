89a0d2bd39422bf805156a6cabc94fe19740c738
Ian Juma <ian.juma@yahoo.com>
2014-08-28 10:19:55 +0300

    email validation

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,3 +1,8 @@
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 $('#loginData').click( function(evnt) {
         evnt.preventDefault();
 
@@ -7,24 +12,27 @@ $('#loginData').click( function(evnt) {
         email = $('#email').val();
         username = $('#getmobileNo').val(); // verification
 
+        if (!validateEmail(email)) {
+          alert('Please enter a valid email');
+        }
+
         if ($('#getmobileNo').val() == "") {
           alert('You need a valid username');
           return 0;
         }
 
         if ($('#getmobileNo').val().length < 3) {
-          alert('Your username is too short');
+          alert('Your username is too short, at least 5 characters');
           return 0;
         }
 
-
         if ($('#pw').val() == "") {
-          alert('You need a valid password');
+          alert('Password field is empty');
           return 0;
         }
 
         if (passd == "" || passd.length < 5) {
-          alert("Your password is too short or empty");
+          alert("Your password is too short or empty, at least 6 characters");
           return 0;
         }
 
@@ -63,6 +71,16 @@ $('#signInData').click( function(evnt) {
   
   username = $('#mobileNo').val();
   passwd = $('#getPass').val();
+
+  if ( username == "" ) {
+    alert('You need a valid username');
+    return 0;
+  }
+
+  if ( passwd == "" ) {
+    alert('Password cannot be empty');
+    return 0;
+  }
 
 
   data = { "username": username, "password": passwd };
