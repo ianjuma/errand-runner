55d1dfb6262342ec725393f3b7c5dd75f3791f0b
Ian Juma <ian.juma@yahoo.com>
2014-08-29 02:29:43 +0300

    debug

--- runserver.py
+++ runserver.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     # import newrelic.agent
     # newrelic.agent.initialize('conf/newrelic.ini')
-    app.run('127.0.0.1', port=port, debug=True, threaded=False, use_reloader=True)
+    app.run('127.0.0.1', port=port, debug=False, threaded=False, use_reloader=True)
     # app.run(port=8000, debug=True, host='0.0.0.0')
     # this can be omitted if using gevent wrapped around gunicorn
