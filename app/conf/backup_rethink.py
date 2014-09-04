c58acfe9f199f9342be485f04f4363f4c6b4782b
Ian Juma <ian.juma@yahoo.com>
2014-09-04 14:18:42 +0300

    rethink fix

---
+++ app/conf/backup_rethink.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import os
+
+
+def check_dir_exist(os_dir):
+    if not os.path.exists(os_dir):
+        exit(1)
+
+
+def backup():
+    check_dir_exist('/root/rethink_backup/')
+    with  os.chdir('/root/rethink_backup/'):
+        args = ("-a", "taskwetu_db**//")
+        os.execl("/usr/local/bin/rethinkdb-dump", args)
+
+if __name__ == 'main':
+    backup()
